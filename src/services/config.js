9a938a97ba7d147090ed1f18cac7aad21927b0bd
gnunesns <nunesguilherme920@gmail.com>
2022-09-09 10:54:32 -0300

    modification api config

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-    baseURL: 'http://localhost:8080/api/v1/'
+    baseURL: 'http://bookstore-apirestful.herokuapp.com/api/v1/'
     
 })
 
